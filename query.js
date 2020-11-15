c7404a270b25137a5668ae624bd6699b22a0e256
Andre Pato <anpato1994@gmail.com>
2020-11-15 15:24:35 -0500

    update starter code

--- query.js
+++ query.js
@@ -1,109 +1,54 @@
 const { User, Task } = require('./models')
-const Sequelize = require('sequelize');
+const Sequelize = require('sequelize')
 const Op = Sequelize.Op
 
 // Find all users with their associated tasks
 // Raw SQL: SELECT * FROM "Users" JOIN tasks ON "Tasks"."userId" = "Users".id;
 
-const findAllWithTasks = async () => {
-    const users = await User.findAll({
-        include: [{
-            model: Task
-        }]
-    });
-    console.log("All users with their associated tasks:", JSON.stringify(users, null, 4));
-}
+const findAllWithTasks = async () => {}
 
-const findAllJohnsWithTasks = async () => {
-    const users = await User.findAll({
-        include: [{
-            model: Task,
-            where: { title: "Work on Unit 3." }
-        }]
-    });
-    console.log("All users with their associated tasks:", JSON.stringify(users, null, 4));
-}
+const findAllJohnsWithTasks = async () => {}
 
 // Find all users
 // Raw SQL: SELECT * FROM "Users";
 
-const findAll = async () => {
-    const users = await User.findAll();
-    console.log("All users:", JSON.stringify(users, null, 4));
-}
+const findAll = async () => {}
 
 // Create a new user
 // Raw SQL: INSERT INTO users (id, firstName, lastName, email, password) VALUES (DEFAULT, 'Jane', 'Doe', 'jane@jane.com', '321cba')
-const createUser = async () => {
-    const jane = await User.create({ firstName: "Jane", lastName: "Doe", email: "jane@jane.com" })
-    console.log("Jane's auto-generated ID:", jane.id)
-}
-
+const createUser = async () => {}
 
 // Delete everyone named "Jane"
 // Raw SQL: DELETE FROM users WHERE firstName = 'Jane'
-const destroyUser = async () => {
-    const destroyed = await User.destroy({
-        where: {
-            firstName: "Jane"
-        }
-    })
-    console.log("Destroyed:", destroyed);
-}
+const destroyUser = async () => {}
 
 // Change lastname "Doe" to "Smith"
 // UPDATE users SET lastName='Smith' WHERE lastName = 'Doe'
-const updateUser = async () => {
-    const updated = await User.update({ lastName: "Smith" }, {
-        where: {
-            lastName: "Doe"
-        }
-    })
-    console.log("Updated:", updated);
-}
+const updateUser = async () => {}
 
 // Find all users and only show their email
 // Raw SQL: SELECT email FROM users;
-const findAllEmails = async () => {
-    const emails = await User.findAll({
-        attributes: ['email']
-    })
-    console.log("All user emails:", JSON.stringify(emails, null, 4));
-}
+const findAllEmails = async () => {}
 
 // Find all users where firstname is John
 // Raw SQL: SELECT * FROM users WHERE firstName = "John";
-const findAllJohns = async () => {
-    const johns = await User.findAll({
-        where: {
-            firstName: "John"
-        }
-    })
-    console.log("All users with first name John:", JSON.stringify(johns, null, 4));
-}
+const findAllJohns = async () => {}
 
 // Find all users where firstname is either John or Jane
 // Raw SQL: SELECT * FROM user WHERE firstName = "John" OR firstName = "Jane";
-const findAllJohnsOrJanes = async () => {
-    const johnOrJanes = await User.findAll({
-        where: {
-            [Op.or]: [{ firstName: "John" }, { firstName: "Jane" }]
-        }
-    })
-    console.log("All users with first name John or Jane:", JSON.stringify(johnOrJanes, null, 4));
-}
+const findAllJohnsOrJanes = async () => {}
 
 const run = async () => {
-    await findAllWithTasks()
-    // await findAllJohnsWithTasks()
-    // await findAll()
-    // await createUser()
-    // await destroyUser()
-    // await updateUser()
-    // await findAllEmails()
-    // await findAllJohns()
-    // await findAllJohnsOrJanes()
-    await process.exit()
+  // await findAllWithTasks()
+  // await findAllJohnsWithTasks()
+  // await findAll()
+  // await createUser()
+  // await destroyUser()
+  // await updateUser()
+  // await findAllEmails()
+  // await findAllJohns()
+  // await findAllJohnsOrJanes()
+  await process.exit()
 }
 
 run()
